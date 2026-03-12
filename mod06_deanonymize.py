06ad8eb0739f3425f64f2922cb9605ebe54e0441
Logan <lscala9@icloud.com>
2026-03-11 21:18:17 -0400

    Homework Solutions

--- mod06_deanonymize.py
+++ mod06_deanonymize.py
@@ -10,20 +10,24 @@ def load_data(anonymized_path, auxiliary_path):
 
 
 def link_records(anon_df, aux_df):
-    """
-    Attempt to link anonymized records to auxiliary records
-    using exact matching on quasi-identifiers.
-
-    Returns a DataFrame with columns:
-      anon_id, matched_name
-    containing ONLY uniquely matched records.
-    """
-    raise NotImplementedError
-
+    final_matches = []
+    for i in range(len(anon_df)):
+        current_anon = anon_df.iloc[i] 
+        matches_found = []
+        for j in range(len(aux_df)):
+            current_aux = aux_df.iloc[j]
+            if current_anon['age'] == current_aux['age']:
+                if current_anon['gender'] == current_aux['gender']:
+                    if current_anon['zip3'] == current_aux['zip3']:
+                        matches_found.append(current_aux['name'])
+        if len(matches_found) == 1:
+            match_data = {
+                'anon_id': current_anon['anon_id'],
+                'matched_name': matches_found[0]
+            }
+            final_matches.append(match_data)
+    return pd.DataFrame(final_matches)
 
 def deanonymization_rate(matches_df, anon_df):
-    """
-    Compute the fraction of anonymized records
-    that were uniquely re-identified.
-    """
-    raise NotImplementedError
+        
+    return len(matches_df)/len(anon_df)
